6bf66e5327062e9e5bd93e23a0e953532f970510
ptroshenkov <pavel.troshenkov@booking.com>
2020-05-04 13:27:05 +0300

    fixing pickle problems

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,83 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn import svm
+from sklearn.feature_extraction.text import TfidfVectorizer
 from flask import Flask, jsonify, request, render_template
 import random
 import pandas as pd
-from lib.classifier import *
 from joblib import load
+import re
+import warnings
+import pickle
+
+warnings.filterwarnings('ignore')
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 clf = load('/var/www/src/model/mymed_v0.joblib')
 pcp_dict = load('/var/www/src/model/pcp_dict.joblib')
+
+
+def text_normalize(x):
+    return ' '.join(r for r in re.findall(r'[а-я]+', str(x).lower())
+                    if len(r) > 2)
+
+
+def top_pair(values, keys, n=3):
+    return sorted(zip(values, keys), reverse=True)[:n]
+
+
+def predict_diag(model, X):
+    predictions = model.predict_proba(X)
+    classes = model.classes_
+    return [top_pair(p, classes) for p in predictions]
+
+
+class ItemSelector(BaseEstimator, TransformerMixin):
+    """For data grouped by feature, select subset of data at a provided key.
+
+    The data is expected to be stored in a 2D data structure, where the first
+    index is over features and the second is over samples.  i.e.
+
+    >> len(data[key]) == n_samples
+
+    Please note that this is the opposite convention to scikit-learn feature
+    matrixes (where the first index corresponds to sample).
+
+    ItemSelector only requires that the collection implement getitem
+    (data[key]).  Examples include: a dict of lists, 2D numpy array, Pandas
+    DataFrame, numpy record array, etc.
+
+    >> data = {'a': [1, 5, 2, 5, 2, 8],
+               'b': [9, 4, 1, 4, 1, 3]}
+    >> ds = ItemSelector(key='a')
+    >> data['a'] == ds.transform(data)
+
+    ItemSelector is not designed to handle data grouped by sample.  (e.g. a
+    list of dicts).  If your data is structured this way, consider a
+    transformer along the lines of `sklearn.feature_extraction.DictVectorizer`.
+
+    Parameters
+    ----------
+    key : hashable, required
+        The key corresponding to the desired value in a mappable.
+    """
+
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_dict):
+        if self.key == 'symptomps':
+            return data_dict[self.key]
+        else:
+            return data_dict[self.key].values.reshape(-1, 1)
 
 
 def check_age(age):
@@ -64,7 +133,7 @@ def predict(text):
                          'age': [age],
                          'gender': [gender]})
 
-    prediction = pd.DataFrame(predict_diag(clf, data)[0], columns=['Вероятность', 
+    prediction = pd.DataFrame(predict_diag(clf, data)[0], columns=['Вероятность',
                                                                    'Болезнь'])
     prediction['Доктор'] = prediction['Болезнь'].map(pcp_dict)
 
